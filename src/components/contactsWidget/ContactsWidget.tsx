92ed486624bc53d93145aee20598b75b0b6062df
Dimkles <dimkless.sharabaiko@gmail.com>
2022-11-18 19:13:30 +0300

    Добавил виджет с контактами

--- src/components/contactsWidget/ContactsWidget.tsx
+++ src/components/contactsWidget/ContactsWidget.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './ContactsWidget.scss'
-const ContactsWidget = () => {
+const ContactsWidget: FC = () => {
     return (
-        <div className='contsctsWidget'>
-            <a target={'_blank'} type='tel' href="+7 (908) 099 55-41" className='contsctsWidget__link contsctsWidget__link--tel'></a>
-            <a target={'_blank'} href="" className='contsctsWidget__link contsctsWidget__link--tg'></a>
-            <a target={'_blank'} href="" className='contsctsWidget__link contsctsWidget__link--vk'></a>
-            <a target={'_blank'} href="" className='contsctsWidget__link contsctsWidget__link--github'></a>
+        <div className='contactsWidget'>
+            <ul className='contactsWidget__list'>
+                {/* <li className='contactsWidget__item'>
+                    <a target={'_blank'} type='tel' href="+7 (908) 099 55-41" className='contsctsWidget__link contsctsWidget__link--tel'>
+                        +7 (908) 099 55-41
+                    </a>
+                </li> */}
+                <li>
+                    <a target={'_blank'} href="https://t.me/Dimkless3034" className='contsctsWidget__link contsctsWidget__link--tg'>
+                        <img src="./img/tg.svg" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a target={'_blank'} href="https://vk.com/dimkless3034" className='contsctsWidget__link contsctsWidget__link--vk'>
+                        <img src="./img/vk.svg" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a target={'_blank'} href="https://github.com/Dimkles" className='contsctsWidget__link contsctsWidget__link--github'>
+                        <img src="./img/github.svg" alt="" />
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
