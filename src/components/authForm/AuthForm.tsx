cc947e384bb528f1d37d784bbfab607cf9c9792d
Dimkles <dimkless.sharabaiko@gmail.com>
2022-12-07 11:04:19 +0300

    обработка ошибок, кастомная ошибка

--- src/components/authForm/AuthForm.tsx
+++ src/components/authForm/AuthForm.tsx
@@ -1,30 +1,46 @@
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+
 import React, { FC, useState } from 'react';
+import { CustomErrorType, isCustomError } from '../../service/RTK/service';
 import { useLoginMutation } from '../../service/RTK/UserService';
 import MyButton from '../myButton/MyButton';
 import MyInput from '../myInput/MyInput';
 import './AuthForm.scss'
 
 interface AuthFormProps {
-    onSubmit?: any
+    setModalActive?: any
 }
 
-const AuthForm: FC<AuthFormProps> = ({ onSubmit }) => {
+const AuthForm: FC<AuthFormProps> = ({ setModalActive }) => {
+    const [isRegistration, setIsRegistration] = useState(false)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, serError] = useState('')
     const [login] = useLoginMutation()
     const submitHandler = async (e: React.FormEvent) => {
         e.preventDefault()
-        const { token } = await login({ email, password }).unwrap()
-        localStorage.setItem('token', token)
-        onSubmit(false)
+        try {
+            const { token } = await login({ email, password }).unwrap()
+            localStorage.setItem('token', token)
+            setModalActive(false)
+        } catch (e) {
+            if ((isCustomError(e))) {
+                serError(e.data.message)
+            }
+        }
+    }
+
+    const changeFormHandler = (e: React.FormEvent) => {
+        serError('')
     }
 
 
     return (
-        <form onSubmit={submitHandler} className='authForm'>
+        <form onChange={changeFormHandler} onSubmit={submitHandler} className='authForm'>
             <MyInput name='Email' type='email' placeholder='Введите email' value={email} setValue={setEmail} />
             <MyInput name='Пароль' type='password' placeholder='Введите пароль' value={password} setValue={setPassword} />
             <MyButton type='submit'>Войти</MyButton>
+            {error && <span className='authForm__error'>{error}</span>}
         </form>
     );
 };
--- src/service/RTK/service.ts
+++ src/service/RTK/service.ts
@@ -1,6 +1,20 @@
 import { BaseQueryFn, FetchArgs, fetchBaseQuery, FetchBaseQueryError } from '@reduxjs/toolkit/dist/query/react'
+
 import { Mutex } from 'async-mutex'
 import { BACKEND_URL } from '../../constants'
+
+export function isCustomError(
+    error: unknown
+): error is CustomErrorType {
+    return typeof error === 'object' && error != null && 'status' in error
+}
+export interface CustomErrorType {
+    status: number
+    data: {
+        message: string
+    }
+}
+
 const mutex = new Mutex()
 export const baseQuery = fetchBaseQuery({
     baseUrl: BACKEND_URL,
@@ -18,29 +32,29 @@ export const baseQuery = fetchBaseQuery({
 export const baseQueryWithReauth: BaseQueryFn<
     string | FetchArgs,
     unknown,
-    FetchBaseQueryError, any
+    FetchBaseQueryError | CustomErrorType, any
 > = async (args, api, extraOptions) => {
     await mutex.waitForUnlock()
     let result = await baseQuery(args, api, extraOptions)
     if (result.error && result.error.status === 401) {
-        if (!mutex.isLocked()) {
-            const release = await mutex.acquire()
-            try {
-                const refreshResult: any = await baseQuery('/auth/refresh', api, extraOptions)
-                if (refreshResult.data.token) {
-                    localStorage.setItem('token', refreshResult.data.token)
-
-                    console.log(refreshResult.data)
-                    result = await baseQuery(args, api, extraOptions)
-                } else {
-                    console.log('logout')
+        if (localStorage.getItem('token')) {
+            if (!mutex.isLocked()) {
+                const release = await mutex.acquire()
+                try {
+                    const refreshResult: any = await baseQuery('/auth/refresh', api, extraOptions)
+                    if (refreshResult.data.token) {
+                        localStorage.setItem('token', refreshResult.data.token)
+                        result = await baseQuery(args, api, extraOptions)
+                    } else {
+                        console.log('logout')
+                    }
+                } finally {
+                    release()
                 }
-            } finally {
-                release()
+            } else {
+                await mutex.waitForUnlock()
+                result = await baseQuery(args, api, extraOptions)
             }
-        } else {
-            await mutex.waitForUnlock()
-            result = await baseQuery(args, api, extraOptions)
         }
     }
     return result
